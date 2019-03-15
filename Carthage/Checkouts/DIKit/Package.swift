55353a1ec36f18bd6c08bd0ed48f91bd2f2e1883
skondo-quo <s.kondo@quo-digital.jp>
2019-03-15 18:45:24 +0900

    Add Carthage

---
+++ Carthage/Checkouts/DIKit/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+  name: "DIKit",
+  products: [
+    .executable(name: "dikitgen", targets: ["dikitgen"]),
+    .library(name: "DIKit", targets: ["DIKit"]),
+    .library(name: "DIGenKit", targets: ["DIGenKit"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.21.2"),
+  ],
+  targets: [
+    .target(name: "DIKit"),
+    .target(name: "DIGenKit", dependencies: ["DIKit", "SourceKittenFramework"]),
+    .target(name: "dikitgen", dependencies: ["DIGenKit"]),
+    .testTarget(name: "DIGenKitTests", dependencies: ["DIGenKit"])
+  ]
+)
