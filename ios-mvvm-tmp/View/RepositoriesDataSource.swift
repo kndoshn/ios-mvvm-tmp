28af568d6582267a564daba4891a92b375724a46
kndoshn <4eswaq@gmail.com>
2019-03-19 23:57:13 +0900

    RepositoriesDataSource

--- ios-mvvm-tmp/View/RepositoriesDataSource.swift
+++ ios-mvvm-tmp/View/RepositoriesDataSource.swift
@@ -3,4 +3,45 @@ import DataSourceKit
 import RxSwift
 import RxCocoa
 
+final class RepositoriesDataSource: CollectionViewDataSource<RepositoriesViewModel.CellDeclaration> {
+    private let starredIndexRelay = PublishRelay<Int>()
 
+    private(set) lazy var starredIndex = starredIndexRelay.asSignal()
+
+    init() {
+        super.init { declaration in
+            switch declaration {
+            case .empty(let isLoading):
+                return EmptyCell.makeBinder(value: isLoading)
+            case .repository(let repository):
+                return RepositoryCell.makeBinder(value: repository)
+            }
+        }
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAt: indexPath)
+        switch cell {
+        case let cell as RepositoryCell:
+            cell.starButtonTap
+                .emit(onNext: { [starredIndexRelay] in
+                    starredIndexRelay.accept(indexPath.row)
+                })
+                .disposed(by: cell.reuseDisposeBag)
+        default:
+            break
+        }
+        return cell
+    }
+}
+
+extension RepositoriesDataSource: RxCollectionViewDataSourceType {
+    func collectionView(_ collectionView: UICollectionView, observedEvent: Event<RepositoriesDataSource.Element>) {
+        guard case .next(let newCellDeclarations) = observedEvent else {
+            return
+        }
+
+        cellDeclarations = newCellDeclarations
+        collectionView.reloadData()
+    }
+}
