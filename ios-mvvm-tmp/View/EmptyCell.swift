a6213ecb4ab1e91edfb5578b58d77ccb37fdad7a
kndoshn <4eswaq@gmail.com>
2019-03-19 23:31:51 +0900

    EmptyCell

--- ios-mvvm-tmp/View/EmptyCell.swift
+++ ios-mvvm-tmp/View/EmptyCell.swift
@@ -1,5 +1,30 @@
 import UIKit
+import DataSourceKit
 
 final class EmptyCell: UICollectionViewCell {
+    @IBOutlet private weak var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet private weak var messageLabel: UILabel!
 
+    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        layoutAttributes.size = superview?.frame.size ?? UIScreen.main.bounds.size
+        return layoutAttributes
+    }
+}
+
+extension EmptyCell: BindableCell {
+    static func makeBinder(value isLoading: Bool) -> CellBinder {
+        return CellBinder(
+            cellType: EmptyCell.self,
+            registrationMethod: .none,
+            reuseIdentifier: "EmptyCell",
+            configureCell: { cell in
+                cell.messageLabel.isHidden = isLoading
+
+                if isLoading {
+                    cell.activityIndicatorView.startAnimating()
+                } else {
+                    cell.activityIndicatorView.stopAnimating()
+                }
+            })
+    }
 }
